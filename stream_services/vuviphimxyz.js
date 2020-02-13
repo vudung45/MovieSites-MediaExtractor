1a3d980d5a87350b53bc1d374dd0c2732af39eba
vudung45 <vudung45@yahoo.com>
2020-02-13 11:15:25 -0600

    u tell me this isn't clean code???

--- stream_services/vuviphimxyz.js
+++ stream_services/vuviphimxyz.js
@@ -21,45 +21,37 @@ class VuViPhimStream extends StreamingService {
         let referer = getProp(aux, "referer", BASE_URL);
 
         let src = null;
-        if(this.cacheManager != null)
-            src = await this.cacheManager.load(this._prefixifyData(JSON.stringify(aux)));
-        
-        if(src == null) {
-            try {
-                let urlResp = await request(url, {
-                    headers: {
-                        "Content-type" : "application/x-www-form-urlencoded; charset=UTF-8",
-                        "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
-                        "Origin": origin,
-                        "Referer": referer,
-                        "Accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,vi;q=0.6",
-                    }
-                });
-                let sourcesRegex = urlResp.body.match(/(eval\(function\(p,a,c,k,e,d\).*?)\s+?<\/script>/);
-                let sources = null;
-                if(sourcesRegex.length) {
-                    sources = sourcesRegex[1];
-                    sources = unpackJS(sources);
-                } else {
-                    sources = urlResp.body;
+        try {
+            let urlResp = await request(url, {
+                headers: {
+                    "Content-type" : "application/x-www-form-urlencoded; charset=UTF-8",
+                    "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
+                    "Origin": origin,
+                    "Referer": referer,
+                    "Accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,vi;q=0.6",
                 }
-                sourcesRegex = sources.match(/sources: *( *?\[.*?\])/);
-                if(sourcesRegex.length) {
-                    sources = sourcesRegex[1].replace(/(?<={|,)([a-zA-Z][a-zA-Z0-9]*)(?=:)'/, "");
-                    let jsonSources = null;
-                    eval(`jsonSources = ${sources};`);;
-                    if(jsonSources.length){
-                        src = []
-                        jsonSources.map(m => src.push(new MediaSource(getProp(m, "file"), getProp(m, "type"), getProp(m, "label"))));
-                    }
-                }   
-            } catch (e) {
-                console.log("Error while getting vuviphim.xyz media source");
+            });
+            let sourcesRegex = urlResp.body.match(/(eval\(function\(p,a,c,k,e,d\).*?)\s+?<\/script>/);
+            let sources = null;
+            if(sourcesRegex.length) {
+                sources = sourcesRegex[1];
+                sources = unpackJS(sources);
+            } else {
+                sources = urlResp.body;
             }
+            sourcesRegex = sources.match(/sources: *( *?\[.*?\])/);
+            if(sourcesRegex.length) {
+                sources = sourcesRegex[1].replace(/(?<={|,)([a-zA-Z][a-zA-Z0-9]*)(?=:)'/, "");
+                let jsonSources = null;
+                eval(`jsonSources = ${sources};`);;
+                if(jsonSources.length){
+                    src = []
+                    jsonSources.map(m => src.push(new MediaSource(getProp(m, "file"), getProp(m, "type"), getProp(m, "label"))));
+                }
+            }   
+        } catch (e) {
+            console.log("Error while getting vuviphim.xyz media source");
         }
-        if(src && this.cacheManager)
-            this.cacheManager.update(JSON.stringify(aux), src);
-
         return src;
     }
 }
